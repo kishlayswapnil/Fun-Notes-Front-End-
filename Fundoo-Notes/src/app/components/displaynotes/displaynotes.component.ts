b40092d307c00cdc9ca10b4920b883a595bb731e
kishlayswapnil <kishlayswapnil@gmail.com>
2020-06-06 08:43:45 +0530

    Refactored: Display note component has been added

--- Fundoo-Notes/src/app/components/displaynotes/displaynotes.component.ts
+++ Fundoo-Notes/src/app/components/displaynotes/displaynotes.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Note } from '../model/Note.model';
+import { Note } from '../../model/Note.model';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { NoteService } from '../service/note.service';
+import { NoteService } from '../../service/note.service';
 
 @Component({
   selector: 'app-displaynotes',
@@ -11,36 +11,23 @@ import { NoteService } from '../service/note.service';
 })
 export class DisplaynotesComponent implements OnInit {
   notes = new Array<Note>();
-
+  view: any;
+  private param: any;
 
   constructor(private router: ActivatedRoute,
     private matSnackBar: MatSnackBar,
     private noteService: NoteService) { }
-  searchNotes: any;
-  private param: any;
+
 
   ngOnInit(): void {
-    this.noteService.autoRefresh.subscribe(() => {
-      this.getOtherNotes();
-    });
-    this.router.queryParams.subscribe(params => {
-      this.param = params['note'];
-      this.getOtherNotes();
-    });
   }
-  getOtherNotes() {
 
-    this.noteService.getAllNotes().subscribe(
-
+  getView() {
+    this.noteService.getView().subscribe(
       (response: any) => {
-        console.log("response", response);
-        console.log("notes:", response.object);
-        this.notes = response['obj'];
-        this.notes.map(note => this.notes.push(note));
-      },
-      (error: any) => {
-        this.matSnackBar.open("Note was not fetched", "failed", { duration: 5000 })
+        this.view = response.view;
       }
     );
+
   }
 }
