3c89700f47e7cfc4cfa91e07657fd6fa960a5465
kishlayswapnil <kishlayswapnil@gmail.com>
2020-06-06 08:38:26 +0530

    Added: Note card component has been added

--- Fundoo-Notes/src/app/components/note/note.component.ts
+++ Fundoo-Notes/src/app/components/note/note.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Note } from '../model/Note.model';
+import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { NoteService } from 'src/app/service/note.service';
 
 @Component({
   selector: 'app-note',
@@ -7,10 +8,34 @@ import { Note } from '../model/Note.model';
   styleUrls: ['./note.component.scss']
 })
 export class NoteComponent implements OnInit {
-  @Input() note: Note;
+  data: any[];
 
-  constructor() { }
-  ngOnInit() {
+  constructor(private matSnackBar: MatSnackBar,
+    private noteService: NoteService) { }
+  ngOnInit(): void {
+    this.getNotes();
+  }
+
+  getNotes() {
+    this.noteService.getRequestNote('getall').subscribe(
+      response => {
+        console.log(response)
+        this.data = response['body']
+        console.log("info=====", this.data)
+      },
+      error => {
+        console.log('Error', error);
+      }
+    )
+  }
+
+  removeReminder(id: any) {
+    this.noteService.removeReminder(id, null).subscribe(
+      (response: any) => {
+        console.log("response : ", response);
+        this.matSnackBar.open("Reminder Removed", "Ok", { duration: 4000 })
+      }
+    )
   }
 }
 
